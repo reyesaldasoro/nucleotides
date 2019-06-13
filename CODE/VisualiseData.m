283bcbbbcfbcd1790da97f6d05914caeea0d067f
CCRA <ccr22@cm3-1-01-160.managed.city.ac.uk>
2019-06-13 12:05:03 +0100

    Update VisualiseData.m
    Add option to run in windows or Mac

--- CODE/VisualiseData.m
+++ CODE/VisualiseData.m
@@ -1,8 +1,16 @@
 clear all
 close all
+%%
 
-cd ('D:\OneDrive - City, University of London\Acad\Research\Birmingham_Nucleotides');
-
+%% Read the files that have been stored in the current folder
+if strcmp(filesep,'/')
+    % Running in Mac
+        cd ('/Users/ccr22/OneDrive - City, University of London/Acad/Research/Birmingham_Nucleotides')
+else
+    % running in windows
+    cd ('D:\OneDrive - City, University of London\Acad\Research\Birmingham_Nucleotides');
+    
+end
 %%
 
 dir0 = dir('tr*.mat');
